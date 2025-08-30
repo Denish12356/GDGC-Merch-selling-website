446241b007fc83d9a56bd82db88090e2bd319c2b
Denish Bharadwaj <denishbharadwaj050@gmail.com>
2025-08-30 23:28:22 +0530

    Update and rename order-form.js to product.js

--- product.js
+++ product.js
@@ -2,16 +2,19 @@ const increase = document.querySelector('.plus');
 const decrease = document.querySelector('.minus');
 const qtySpan = document.querySelector('.qty');
 
-let qty = 1;
+let qty = parseInt(localStorage.getItem("qty")) || 1;
+qtySpan.textContent = qty;
 
 increase.addEventListener('click', () => {
     qty++;
     qtySpan.textContent = qty;
+    localStorage.setItem("qty", qty);
 })
 
 decrease.addEventListener('click', () => {
     if (qty > 1) {
         qty--;
         qtySpan.textContent = qty;
+        localStorage.setItem("qty", qty);
     }
 })
